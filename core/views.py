8a8f98f85f52c86e5b74501f0eaa2c430e67a2a4
david de la puente enriquez <Twinkymen@hotmail.com>
2025-04-29 05:44:21 +0200

    Editando Template tags de tipo blog y extends.

--- core/views.py
+++ core/views.py
@@ -12,27 +12,13 @@ html_base = """
 
 # Create your views here.
 def home(request):
-    return HttpResponse(html_base + """
-            <h2>Portada</h2>
-            <p>Esto es la portada</p>
-    """)
+    return render(request, "core/home.html")
 
 def about(request):
-    return HttpResponse(html_base + """
-            <h2>Acerca de</h2>
-            <p>Me llamo David y soy programador.</p>
-    """)
+    return render(request, "core/about.html")
 
 def portfolio(request):
-    return HttpResponse(html_base + """
-            <h2>Portafolio</h2>
-            <p>Este es mí portafolio aqui iré subiendo los proyectos que vaya
-                desarrollando en las diferentes technologias que domino para que 
-                podais conocerme un poco mejor.</p>
-    """)
+    return render(request, "core/portfolio.html")
 
 def contact(request):
-    return HttpResponse(html_base + """
-            <h2>Contacto</h2>
-            <p>Formulario de Contacto</p>                 
-    """)
+    return render(request, "core/contacto.html")
